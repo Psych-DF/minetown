3d2f4194150eba4b91de52761f4fbeeb482a4eb7
Psych-DF <95335322+Psych-DF@users.noreply.github.com>
2025-05-02 22:36:36 +1000

    Update tileTypes.js

--- js/tileTypes.js
+++ js/tileTypes.js
@@ -3,6 +3,5 @@ export function getRandomTileType() {
 
   if (rand < 0.05) return "gold";
   if (rand < 0.20) return "ore";
-  if (rand < 0.25) return "void";  // 5% chance: missing tile
   return "dirt";
 }
